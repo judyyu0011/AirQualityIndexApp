9cc39cb72c4cd1d7e157c066b6642fd658a35dfd
Judy Yu <judyyu0011@gmail.com>
2020-11-07 19:38:23 -0800

    displays aqi + source for 3 cities

--- app.js
+++ app.js
@@ -1,19 +1,35 @@
-// var richmond = document.getElementById('Richmond');
-// var la = document.getElementById('LA');
-// var shanghai = document.getElementById('Shanghai')
-
-
-
-// richmond.onlick = function() {
-//     $.getJSON('https://api.waqi.info/feed/shanghai/?token=53f407f0dab7cf67ad57f71d6c32ad3588cbe32f', function(aqiData) {
-//         document.getElementById("aqi").innerHTML = aqiData.data.city;
-//     });
-//     alert("test");
-// }
-
-function richmondFn() {
-    $.getJSON('https://api.waqi.info/feed/shanghai/?token=53f407f0dab7cf67ad57f71d6c32ad3588cbe32f', function(aqiData) {
-        document.getElementById("aqi").innerHTML = aqiData.data.city;
+function vancouverFn() {
+    $.getJSON('https://api.waqi.info/feed/vancouver/?token=53f407f0dab7cf67ad57f71d6c32ad3588cbe32f', function(aqiData) {
+        displayAQI(aqiData);
     });
-    alert("test");
+    // alert("test");
+}
+
+function laFn() {
+    $.getJSON('https://api.waqi.info/feed/los-angeles/?token=53f407f0dab7cf67ad57f71d6c32ad3588cbe32f', function(aqiData) {
+        displayAQI(aqiData);
+    });
+    // alert("test");
+}
+
+function shanghaiFn() {
+    $.getJSON('https://api.waqi.info/feed/shanghai/?token=53f407f0dab7cf67ad57f71d6c32ad3588cbe32f', function(aqiData) {
+        displayAQI(aqiData);    
+    });
+    // alert("test");
+}
+
+function displayAQI(aqiData) {
+    document.getElementById("aqi").innerHTML = aqiData.data.city.name;
+    const aqiContainer = document.querySelector('#aqi');
+
+    const aqiValue = document.createElement('h1');
+    aqiContainer.append(aqiValue);
+    aqiValue.classList.add('aqi-value');
+    aqiValue.innerHTML = aqiData.data.aqi;
+
+    const source = document.createElement('p');
+    aqiContainer.appendChild(source);
+    source.classList.add('data-source');
+    source.innerHTML = "Source: " + aqiData.data.attributions[0].name;
 }
